72780d0906f9db2499662aca20a503f677042f6f
asmar <elasmaroussama@gmail.com>
2021-10-19 17:37:52 +0100

    add comment to file main

--- src/ClientWs.java
+++ src/ClientWs.java
@@ -3,6 +3,7 @@ import proxy.BanqueWS;
 
 public class ClientWs {
     public static void main(String[] args) {
+        //com
         BanqueService stub = new BanqueWS().getBanqueServicePort();
         System.out.println(stub.convertionEuroToDh(800));
     }
